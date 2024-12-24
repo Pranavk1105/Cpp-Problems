e49fe8de6cb24a5d327ac06b2e124464500686a7
Pranavk1105 <pranavkale2806@gmail.com>
2024-12-24 22:04:17 +0530

    Add files via upload

---
+++ age18.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+            int age;
+            cout << "enter your age:";
+            cin >> age;
+            if (age>18)
+                        {cout << "\n you are eligible to vote";}
+                        else
+                        {cout << "\n not eligible to vote";}
+                        return 0;
+            }
+            
+
